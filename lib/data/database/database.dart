723945e373702b5f267f3d02aacac8793759c77a
Vorkytaka <vorkytaka@gmail.com>
2023-11-07 01:11:04 +0300

    Remove logs

--- lib/data/database/database.dart
+++ lib/data/database/database.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -19,6 +20,6 @@ class Database extends _$Database {
   static QueryExecutor get _connection => LazyDatabase(() async {
         final dbFolder = await getApplicationDocumentsDirectory();
         final file = File(join(dbFolder.path, 'db.sqlite'));
-        return NativeDatabase.createInBackground(file, logStatements: true);
+        return NativeDatabase.createInBackground(file, logStatements: kDebugMode);
       });
 }
